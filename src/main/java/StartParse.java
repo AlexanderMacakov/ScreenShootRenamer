87b186dc875cff22c519cb467f8d6b12ab5e788b
Alexander Macakov <macakovaleksandr@gmail.com>
2018-07-03 09:39:28 +0400

    рефакторинг

--- src/main/java/StartParse.java
+++ src/main/java/StartParse.java
@@ -7,20 +7,11 @@ public class StartParse {
         File[] filesList;
         int count = 1;
         Settings settings = new Settings();
-        File filesPath = new java.io.File("D:\\ScreenShootRenamer\\RefFiles"); // создаем объект на папку с файлами
+        File filesPath = new java.io.File("D:\\ScreenShootRenamer\\RefFiles");
+        filesList = filesPath.listFiles();
 
-        filesList = filesPath.listFiles(); // записываем файлы из папки в массив объектов типа File
+        for (File file: filesList) {
 
-        for (int i = 0; i < filesList.length; i++) {
-            File file = filesList[i];
-            String nameCount = String.valueOf(count);
-            if(file.exists()){ // если файл существует, то переименовываем его
-                file.renameTo( new File(settings.getDate() + String.valueOf(count) ));
-                count++;
-            }
-            else{
-                System.out.println("File not found!");
-            }
         }
     }
 }
