165f327543bbab6c71911d94bd4f8ef56dd6c7ac
Alexander Macakov <macakovaleksandr@gmail.com>
2018-07-04 11:43:22 +0400

    поправил меню, допилил настройки

--- src/main/java/HomeTarget.java
+++ src/main/java/HomeTarget.java
@@ -10,12 +10,47 @@ public class HomeTarget {
         new File("D:\\ScreenShootRenamer").mkdir();
         Faq.context();
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String choise;
 
         while (true) {
-            String choise = reader.readLine();
+            choise = reader.readLine();
             if (choise.equals("1")) {
                 StartParse startParse = new StartParse();
                 startParse.run();
+            }
+            else if (choise.equals("2")) {
+                Settings settings = new Settings();
+                System.out.println(Settings.nameSetings);
+                while (true) {
+
+                    settings.printAbout();
+                    choise = reader.readLine();
+
+                    if (choise.equals("1")) {
+                        System.out.println(settings.getNameDirectory());
+                    }
+                     else if (choise.equals("2")) {
+                        System.out.println("Введите новый путь до директории");
+                        String editPath = reader.readLine();
+                    }
+                    else if (choise.equals("3")) {
+                        break;
+                    } else{
+                        System.out.println("Ля, выбери что-то из меню, другого не дано");
+                    }
+
+                }
+
+            }
+            else if (choise.equals("3")) {
+                System.out.println("В разработке");
+            }
+            else if (choise.equals("4")) {
+                System.out.println("Удачного дня!");
+                break;
+            }
+             else{
+                System.out.println("Ля, выбери что-то из меню, другого не дано");
             }
         }
 
--- src/main/java/Settings.java
+++ src/main/java/Settings.java
@@ -6,27 +6,19 @@ public class Settings {
 
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     static String nameSetings = "Настройки";
-    static String defaultPath = "D:\\ScreenShootRenamer\\Settings";
-    static String userPath;
+    static String path = "D:\\ScreenShootRenamer\\Settings";
+
 
     public Settings() throws FileNotFoundException {
-        printAbout();
     }
 
-    public void getNameDirectory() {
-        if(userPath == null) {
-            System.out.println(defaultPath);
-        }
-        else System.out.println(userPath);
+    public String getNameDirectory() {
+            return path;
     }
 
-    public void editDetictory() {
-        System.out.println("Введите путь к новой директории");
-        try {
-            userPath = reader.readLine();
-        } catch (IOException e) {
-            System.out.println("Введен некорректный путь");
-        }
+    public void editDetictory(String newPath) {
+        path = newPath;
+        System.out.println("Директория изменена.");
     }
 
     public String getDate() {
@@ -37,8 +29,7 @@ public class Settings {
 
 
 
-    private void printAbout() {
-        System.out.println(nameSetings);
+    public void printAbout() {
         System.out.println("1) путь директории");
         System.out.println("2) изменить путь");
         System.out.println("3) Вернуться назад");
