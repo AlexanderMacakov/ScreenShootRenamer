f5975f3c2622616ae07ff308cb557b5ffed79447
Alexander Macakov <macakovaleksandr@gmail.com>
2018-07-10 15:45:46 +0400

    программа теперь работает

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# ScreenShootRenamer
+ScreenShootRenamer
+-----------------
+
+Переименовывавает файлы из директории `/RefFiles` в директорию `/Compiling`
+
+##### Structure project
+
+```
+\bin
+\Compiling
+\RefFiles
+ StartSSR.bat
+```
+
+##### how to start the application
+
+Запутить StartSSR.bat
+
+TODO
+----
+
+0. Изменять директорию через меню
+0. Создавать архив из всех файлов
+0. Сделать рабочими все пункты меню
+
 
--- src/main/java/StartParse.java
+++ src/main/java/StartParse.java
@@ -4,20 +4,25 @@ import java.io.FileNotFoundException;
 
 public class StartParse {
     public void run() throws FileNotFoundException {
+
         Settings settings = new Settings();
         int countFiles = 1;
-        File filesPath = new File("RefFiles");
+        String pathName = "Compiling\\" + settings.getDate() + "-" + countFiles + ".png";
 
+        File filesPath = new File("RefFiles");
         File[] filesList = filesPath.listFiles();
 
 
         for (File file: filesList) {
-            File renameFile = new File("Compiling\\" + settings.getDate() + "-" + countFiles + ".png");
+
+            File renameFile = new File(pathName);
+
             if (file.renameTo(renameFile)) {
                 System.out.println("Файл: " + renameFile.toString() + " обработан");
             }
             else
                 System.out.println("Ошибка");
+
             countFiles++;
         }
     }
